a8a8990370946022cb888fa961a38cee05e10f86
MackeFerret <maxhulting@hotmail.com>
2022-07-27 10:30:59 +0200

    Commit 2

---
+++ Gamejam22/Assets/Scripts/Movement.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] float jumpHeight = 0f;
+    [SerializeField] float speed = 10.0f;
+
+    float inputX;
+
+    LayerMask ground;
+
+    Rigidbody2D rb;
+   
+    public bool canJump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+        rb = GetComponent<Rigidbody2D>();
+
+
+       // float h = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if(canJump == true)
+            {
+                rb.AddForce(new Vector2(-speed * Time.deltaTime,  0));
+            }
+          
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (canJump == true)
+            {
+                rb.AddForce(new Vector2(speed  * Time.deltaTime, 0));
+            }
+          
+        }
+
+            jump();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        canJump = true;
+  
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canJump = false;
+    }
+    void jump()
+    {
+        if(canJump == true)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+            }
+
+            //float h = Input.GetAxisRaw("Horizontal");
+            // float v = Input.GetAxisRaw("Jump");
+            // gameObject.transform.position = new Vector2(transform.position.x + (h * speed), transform.position.y + (v * jumpHeight));
+        }
+        
+
+    }
+}
